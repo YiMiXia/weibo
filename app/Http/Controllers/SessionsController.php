eed1dad2bbc24ead5740484e9a7b6b5492266481
YiMiXia <7687849@qq.com>
2019-01-07 11:05:30 +0800

    Revert "登录时记住我"
    This reverts commit 91ef779c41431d166f31c3171771a44be6fd0e96.

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -19,18 +19,12 @@ class SessionsController extends Controller
         ]);
 
         // if(Auth::attempt(['email'=>$email,'password'=>$password])){
-        if(Auth::attempt($credentials,$request->has('remember'))){
+        if(Auth::attempt($credentials)){
             session()->flash('success','欢迎回来！');
             return redirect()->route('users.show',[Auth::user()]);
         }else{
             session()->flash('danger','抱歉，帐号密码不匹配');
             return redirect()->back()->withInput();
         }
-    }
-
-    public function destroy(){
-        Auth::logout();
-        session()->flash('success', '您已成功退出！');
-        return redirect('login');
     }
 }
